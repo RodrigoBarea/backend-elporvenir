bd9ddfe87d0a72ab8738e757aedcd991b098c94f
RodrigoBarea <rodrigobarea.p@gmail.com>
2025-10-28 19:15:54 -0400

    plugin

--- config/plugins.js
+++ config/plugins.js
@@ -13,9 +13,7 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'cloudinary',
     providerOptions: {
-      cloud_name: env('CLOUDINARY_CLOUD_NAME'),
-      api_key: env('CLOUDINARY_API_KEY'),
-      api_secret: env('CLOUDINARY_API_SECRET'),
+      cloudinary_url: env('CLOUDINARY_URL'), // Usando la variable de entorno completa
     },
   },
 });
